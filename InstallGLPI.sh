05ba6cf8e2de9dacfa4561da39f89267b74ade22
didyeahrock <didyeah.rock@gmail.com>
2021-09-22 17:59:35 +0200

    Install Apache

--- InstallGLPI.sh
+++ InstallGLPI.sh
@@ -12,3 +12,15 @@ CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'glpi';
 GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost';
 FLUSH PRIVILEGES;
 SQL
+# installer Apache
+sudo apt install apache2 php php-mysql libapache2-mod-php
+# intallation de modules PHP
+sudo apt install php-json php-gd php-curl php-mbstring php-cas php-xml php-cli php-imap php-ldap php-xmlrpc php-apcu
+# activation du module a2enmod
+sudo a2enmod rewrite
+# redémarage d'Apache
+sudo systemctl restart apache2
+# modification du fichier apache2.conf 
+sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.default
+sudo cp /home/didier/Documents/InstallGLPI/apache2.conf /etc/apache2/apache2.conf
+
