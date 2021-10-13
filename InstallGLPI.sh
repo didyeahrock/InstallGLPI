862694541b2c829ce5b4883bf742518bef68184c
didyeahrock <didyeah.rock@gmail.com>
2021-10-13 12:24:37 +0200

    quasi final

--- InstallGLPI.sh
+++ InstallGLPI.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 clear
-# date 12-10-2021
+# date 13-10-2021
+# this script will make an unattended install of GLPI, MySql and PHP 
 echo "L'installation de GLPI va commencer"
-# Mise à jour des sources
-# sudo apt update && sudo apt upgrade
-# Installation de mysql server et client
+# update and upgrade the packets
+sudo apt update && sudo apt upgrade
+# Install of  mysql server and client
+echo "mysql-server mysql-server/root_password password P@ssw0rd" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password P@ssw0rd" | debconf-set-selections
 sudo apt install -y mysql-server mysql-client
 # mysql-creation table, user et droits
 mysql -u root -p <<SQL
@@ -13,57 +16,41 @@ CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'glpi';
 GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost';
 FLUSH PRIVILEGES;
 SQL
-# installer Apache
+# install of Apache server
 sudo apt install -y apache2 php php-mysql libapache2-mod-php
-# intallation de modules PHP
+# intall of PHP  modules 
 sudo apt install -y php-json php-gd php-curl php-mbstring php-cas php-xml php-cli php-imap php-ldap php-xmlrpc php-apcu
 sudo apt-get install -y php7.4-intl
 sudo apt-get install -y php7.4-bz2
 sudo apt-get install -y php7.4-zip
 
 
-# activation du module a2enmod
+# activation of the  a2enmod module
 sudo a2enmod rewrite
-# redémarage d'Apache
-sudo systemctl reload apache2
-# modification du fichier apache2.conf 
+# modification of the default apache2.conf file
 sudo cp -v /etc/apache2/apache2.conf /etc/apache2/apache2.conf.default
 sudo cp -v apache2.conf /etc/apache2/apache2.conf
 sudo cp -v glpi.conf /etc/apache2/conf-available/
-# DEBUT conf PHP
-# D ici à l etiquette  FIN CONF PHP, à trouver
-# Recherchez l’emplacement du fichier de configuration PHP (installez au préalable mlocate et locate) :
-# sudo updatedb
-# locate php.ini
-# Editez le fichier de configuration php.ini :
-# sudo nano /etc/php/7.4/apache2/php.ini
-# Quelques recommandations :
-# file_uploads = On
-# max_execution_time = 300
-# memory_limit = 256M
-# post_max_size = 32M
-# max_input_time = 60
-# max_input_vars = 4440
-# FIN conf PHP
-sudo service apache2 restart
-# téléchargement de la dernière version de GLPI dans /tmp 
+# Apache reload
+sudo systemctl reload apache2
+# download and extract of the last GLPI package to /tmp 
 cd /tmp
 wget https://github.com/glpi-project/glpi/releases/download/9.5.6/glpi-9.5.6.tgz
-# extraction dans /tmp
 tar -zxvf glpi-9.5.6.tgz 
-# Déplacement de GLPI dans vers dossier racine de Apache 
+# Moving /tmp/glpi to the root folder of apache2 
 sudo mv glpi /var/www/html/
-# Atribuer à www-data le contrôle total sur le répertoire GLPI 
+# make www-data owner of GLPI folder
 sudo chown -R www-data /var/www/html/glpi
-# activation de la conf
+# activation of the GLPI configuration
 sudo a2enconf glpi
-# sudo systemctl reload apache2
-# redémarrage du serveur apache
-sudo service apache2 restart 
-# populer la base GLPI/mySQL
+# reload apache
+sudo service apache2 reload
+# populating the GLPI/mySQL database
 mysql -uglpi -pglpi glpi < /var/www/html/glpi/install/mysql/glpi-empty.sql
 sudo chown -R www-data:www-data /var/www/html/glpi/
 sudo php /var/www/html/glpi/bin/console db:install --db-host=127.0.0.1 --db-name=glpi --db-user=glpi --db-password=glpi --force --no-interaction
 sudo chown -R www-data:www-data /var/www/html/glpi/
+# launch Firefox with GLPI home page
+firefox "http://localhost/glpi"
 
 
